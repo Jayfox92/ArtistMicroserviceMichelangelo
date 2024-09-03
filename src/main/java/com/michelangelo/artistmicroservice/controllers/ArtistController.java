e991eed1789c6f2a37d15d3782ce8231383aeda9
EvelinaDaun <evelina.daun@hotmail.com>
2024-09-03 17:53:30 +0200

    artist controller started

---
+++ src/main/java/com/michelangelo/artistmicroservice/controllers/ArtistController.java
@@ -0,0 +1,23 @@
+package com.michelangelo.artistmicroservice.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/artist")
+public class ArtistController {
+
+    // @Autowired
+    // private ArtistService artistService;
+
+    // Endpoint: Hämta alla album för en artist med id och skicka tillbaka en lista med album objekt
+/*
+    @GetMapping("/getAllAlbumsWithArtist/{id}"){
+        public ResponseEntity<List<Album>> getAllAlbums(@PathVariable ("id") int id){
+
+        }
+    }
+*/
+
+
+}
